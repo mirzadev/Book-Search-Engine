682f1412d197997c6341913cb134d17f78207d14
Mirza Abdul Awal <awal.mirza2016@gmail.com>
2021-11-06 10:18:26 -0400

    updating-2

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-
+// rendering
 ReactDOM.render(
-    <App />,  
+  <App />,
   document.getElementById('root')
 );
